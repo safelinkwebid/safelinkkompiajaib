31a96c88fa598038a07b45e4ba6eb924b29d1d40
safelinkwebid <71550781+safelinkwebid@users.noreply.github.com>
2021-05-30 13:35:02 +0700

    Update safelink_utama.js

--- safelink_utama.js
+++ safelink_utama.js
@@ -22,8 +22,8 @@ if(a_to_vh==false)
 var x=Math.floor((Math.random()*4)+ 1);
 var xxx=null;
 if(x=="1"){xxx="post1.html"} 
-if(x=="2"){xxx="post2"} 
-if(x=="3"){xxx="post3"} 
+if(x=="2"){xxx="post2.html"} 
+if(x=="3"){xxx="post3.html"} 
 if(x=="4"){xxx="post4.html"}a_to_ve[i].href="https://kompiajaib.safelink.web.id/2021/05/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
