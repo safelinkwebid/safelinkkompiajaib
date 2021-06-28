28fcff4328a6fff337f310d9b20e6c72a726b3ae
safelinkwebid <71550781+safelinkwebid@users.noreply.github.com>
2021-06-29 02:16:47 +0700

    Update safelinkwebid.js

--- safelinkwebid.js
+++ safelinkwebid.js
@@ -20,7 +20,7 @@ j++;}
 if(a_to_vh==false)
 {var encryptedUrl=Base64.encode(a_to_vi);
 var x=Math.floor((Math.random()*4)+ 1);
-var xxx=what-is-safelink-blogger;
+var xxx="what-is-safelink-blogger";
 if(x=="1"){xxx="what-is-safelink-blogger"}a_to_ve[i].href="https://safelink.web.id/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
